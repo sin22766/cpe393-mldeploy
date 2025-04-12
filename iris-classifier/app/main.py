01c8c7e6b8efbe01efac8b65cf7304d4588ec187
Promchai Chooseang <promchai2sin@gmail.com>
2025-04-13 00:13:04 +0700

    feat: update prediction to be able to handle multiple input

--- iris-classifier/app/main.py
+++ iris-classifier/app/main.py
@@ -2,30 +2,37 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 app = FastAPI()
 
 # Load the trained model
 with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
+    model: RandomForestClassifier = pickle.load(f)
 
 @app.get("/")
 def home():
     return "ML Model is Running"
 
 class PredictInput(BaseModel):
-    features: list
+    features: list[list[float]]
 
 class PredictOutput(BaseModel):
     prediction: int
     confidence: float
 
+
 @app.post("/predict")
-def predict(predict_input: PredictInput) -> PredictOutput:
-    input_features = np.array(predict_input.features).reshape(1, -1)
-    prediction = model.predict(input_features)[0]
-    prediction_proba = model.predict_proba(input_features)[0]
-    return PredictOutput(
-        prediction=int(prediction),
-        confidence=float(prediction_proba[prediction])
-    )
+def predict(predict_input: PredictInput) -> list[PredictOutput]:
+    input_features = np.array(predict_input.features)
+    prediction_probas = model.predict_proba(input_features)
+    
+    result = [PredictOutput(
+        prediction=int(np.argmax(prob)),
+        confidence=float(np.max(prob))
+    ) for prob in prediction_probas]
+
+    return result
+    
+
+    
