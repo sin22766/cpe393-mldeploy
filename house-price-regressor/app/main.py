d77e71221591c573700e885daa6b08ec168f8f80
Promchai Chooseang <promchai2sin@gmail.com>
2025-04-13 21:19:06 +0700

    feat: serve the regression model using fastAPI

--- house-price-regressor/app/main.py
+++ house-price-regressor/app/main.py
@@ -1,6 +1,78 @@
-def main():
-    print("Hello from housepriceregressor!")
+import pickle
+import os
+from fastapi import FastAPI, HTTPException, status
+import pandas as pd
+from pydantic import BaseModel, Field
+from sklearn.ensemble import GradientBoostingRegressor
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "model.pkl")
+
+# Load the trained model
+with open(model_path, "rb") as f:
+    model: GradientBoostingRegressor = pickle.load(f)
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return "ML Model is Running"
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+class Feature(BaseModel):
+    area: int = Field(..., gt=0, description="Area of the house in square units")
+    bedrooms: int = Field(..., ge=0, description="Number of bedrooms")
+    bathrooms: int = Field(..., ge=0, description="Number of bathrooms")
+    stories: int = Field(..., ge=1, description="Number of stories/floors")
+    mainroad: bool = Field(
+        ..., description="Whether the house is connected to a main road"
+    )
+    guestroom: bool = Field(..., description="Whether the house has a guest room")
+    basement: bool = Field(..., description="Whether the house has a basement")
+    hotwaterheating: bool = Field(
+        ..., description="Whether the house has hot water heating"
+    )
+    airconditioning: bool = Field(
+        ..., description="Whether the house has air conditioning"
+    )
+    parking: int = Field(..., ge=0, description="Number of parking spaces")
+    prefarea: bool = Field(..., description="Whether the house is in a preferred area")
+    furnishingstatus: float = Field(
+        ..., ge=0, le=1, description="Status of furnishing in the house"
+    )
+
+
+class PredictInput(BaseModel):
+    features: list[Feature] = Field(..., min_length=1)
+
+
+class PredictOutput(BaseModel):
+    prediction: list[float] = Field(..., description="Predicted price of the house")
+
+
+@app.post("/predict")
+def predict(input: PredictInput) -> PredictOutput:
+    try:
+        # Extract features
+        features = input.model_dump()["features"]
+
+        # Convert to DataFrame
+        df = pd.DataFrame(features)
+
+        # Make predictions
+        predictions = model.predict(df).round(2)
+
+        # Return the prediction
+        return PredictOutput(prediction=predictions.tolist())
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Prediction error: {str(e)}",
+        )
